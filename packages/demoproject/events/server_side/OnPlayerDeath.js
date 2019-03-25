7a9eb338825bdc7d834cb89e70f60aad5409480c
Franze, Mario <mfranze@germandev.de>
2019-03-25 22:45:19 +0100

    * Changed to random spawn after death

--- packages/demoproject/events/server_side/OnPlayerDeath.js
+++ packages/demoproject/events/server_side/OnPlayerDeath.js
@@ -2,6 +2,17 @@ module.exports =
 {
 	'playerDeath' : (player, reason, killer) =>
 	{
-		player.spawn(player.position);
+		const spawnPoints =
+		[
+			{ 'x': 351.0, 'y': -8.2, 'z': 91.4 },
+			{ 'x': 234.7, 'y': -107.2, 'z': 74.5 },
+			{ 'x': 525.2, 'y': 150.0, 'z': 58.7 },
+			{ 'x': 41.3, 'y': 0.0, 'z': 116.1 },
+			{ 'x': -318.1, 'y': -82.7, 'z': 54.1 },
+		];
+		const select = Math.floor(Math.random() * spawnPoints.length);
+		const pos = spawnPoints[select];
+
+		player.spawn(pos);
 	},
 };
