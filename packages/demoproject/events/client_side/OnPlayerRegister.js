a8ec1dc2d9ffa4813c79d731ccfb98c1d742d6a7
Franze, Mario <mfranze@germandev.de>
2019-03-25 18:14:10 +0100

    * Finished registration

--- packages/demoproject/events/client_side/OnPlayerRegister.js
+++ packages/demoproject/events/client_side/OnPlayerRegister.js
@@ -1,7 +1,34 @@
+const mysql = require('mysql2');
+
 module.exports =
 {
 	'OnPlayerRegister' : (player, username, password) =>
 	{
+		const connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			database: 'demoproject',
+		});
+
+		connection.query(
+			'SELECT * FROM `accounts` WHERE `username` = ?', username, (err, results, fields) =>
+			{
+				if(err)
+					return;
+
+				if(results.length > 0)
+					return player.call('UserAllreadyExists');
+
+				connection.query(
+					'INSERT INTO `accounts` (username, password) VALUES (?, ?)', [username, password], (error, res) =>
+					{
+						if(error)
+							return;
+
+						return player.call('RegistrationSuccessfull');
+					});
+			}
+		);
 		// console.log(`Got new data from registration form with username '${username}' and password '${password}'`);
 	},
 };
