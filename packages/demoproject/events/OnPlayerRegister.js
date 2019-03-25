c912b0dfe0966d83dfb1e2b7787d05c4680a5615
Franze, Mario <mfranze@germandev.de>
2019-03-25 07:40:14 +0100

    * Initial commit

---
+++ packages/demoproject/events/OnPlayerRegister.js
@@ -0,0 +1,7 @@
+module.exports =
+{
+	'OnPlayerRegister' : (username, password) =>
+	{
+		console.log(`Got new data from registration form with username '${username}' and password '${password}'`);
+	},
+};
